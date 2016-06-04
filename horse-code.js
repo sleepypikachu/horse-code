bc684eca7ca2e43921e6638e2678e0237fb45544
Elliot Iddon <elliot@citrusbytes.net>
2016-06-04 21:44:29 +0100

    Add ascii to horse code conversion
    New controller with a field which is bound to the editable input
    element and a function for ascii to horse code conversion which
    produces the output of the read only input element.
    
    Uses a function with a static switch statement to return the correct
    horse emoji sequence for a character and returns unicorn for unknown
    characters. Case is not preserved.
    
    Some super lazy css from memory (worked first time!) more interesting
    css to follow.

--- horse-code.js
+++ horse-code.js
@@ -1,7 +1,52 @@
 var horseCodeModule = angular.module('horseCode', []);
 
-horseCodeModule.filter('greet', function() {
-    return function(name) {
-        return 'Hello, ' + name + '!';
+horseCodeModule.controller("horseController", function() {
+    this.ascii;
+
+    this.horseMap = function(character) {
+        switch(character) {
+            case 'a': return "🐎 🐴";
+            case 'b': return "🐴 🐎 🐎 🐎";
+            case 'c': return "🐴 🐎 🐴 🐎";
+            case 'd': return "🐴 🐎 🐎";
+            case 'e': return "🐎";
+            case 'f': return "🐎 🐎 🐴 🐎";
+            case 'g': return "🐴 🐴 🐎";
+            case 'h': return "🐎 🐎 🐎 🐎";
+            case 'i': return "🐎 🐎";
+            case 'j': return "🐎 🐴";
+            case 'k': return "🐴 🐎 🐴";
+            case 'l': return "🐎 🐴 🐎 🐎";
+            case 'm': return "🐴 🐴";
+            case 'n': return "🐴 🐎";
+            case 'o': return "🐴 🐴 🐴";
+            case 'p': return "🐎 🐴 🐴 🐎";
+            case 'q': return "🐴 🐴 🐎 🐴";
+            case 'r': return "🐎 🐴 🐎";
+            case 's': return "🐎 🐎 🐎";
+            case 't': return "🐴";
+            case 'u': return "🐎 🐎 🐴";
+            case 'v': return "🐎 🐎 🐎 🐴";
+            case 'w': return "🐎 🐴 🐴";
+            case 'x': return "🐴 🐎 🐎 🐴";
+            case 'y': return "🐴 🐎 🐴 🐴";
+            case 'z': return "🐴 🐴 🐎 🐎";
+            case ' ': return " / ";
+        }
+        return "🦄";
+    };
+
+    this.horse = function(ascii) {
+        if(ascii != undefined && ascii != "") {
+            return ascii
+                .split('')
+                .map(String.toLowerCase)
+                .map(this.horseMap)
+                .reduce(function (current, previous) {
+                    return current + "   " + previous;
+                });
+        }
+        return "";
     };
 });
+
