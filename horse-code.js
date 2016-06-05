1bc92933abaf55898bbd523335ee720d7c3535df
Elliot Iddon <elliot@citrusbytes.net>
2016-06-05 17:59:30 +0100

    Fix long v.s. short horse, Add fork me on github

--- horse-code.js
+++ horse-code.js
@@ -5,32 +5,32 @@ horseCodeModule.controller("horseController", function() {
 
     this.horseMap = function(character) {
         switch(character.toLowerCase()) {
-            case 'a': return "🐎 🐴";
-            case 'b': return "🐴 🐎 🐎 🐎";
-            case 'c': return "🐴 🐎 🐴 🐎";
-            case 'd': return "🐴 🐎 🐎";
-            case 'e': return "🐎";
-            case 'f': return "🐎 🐎 🐴 🐎";
-            case 'g': return "🐴 🐴 🐎";
-            case 'h': return "🐎 🐎 🐎 🐎";
-            case 'i': return "🐎 🐎";
-            case 'j': return "🐎 🐴";
-            case 'k': return "🐴 🐎 🐴";
-            case 'l': return "🐎 🐴 🐎 🐎";
-            case 'm': return "🐴 🐴";
-            case 'n': return "🐴 🐎";
-            case 'o': return "🐴 🐴 🐴";
-            case 'p': return "🐎 🐴 🐴 🐎";
-            case 'q': return "🐴 🐴 🐎 🐴";
-            case 'r': return "🐎 🐴 🐎";
-            case 's': return "🐎 🐎 🐎";
-            case 't': return "🐴";
-            case 'u': return "🐎 🐎 🐴";
-            case 'v': return "🐎 🐎 🐎 🐴";
-            case 'w': return "🐎 🐴 🐴";
-            case 'x': return "🐴 🐎 🐎 🐴";
-            case 'y': return "🐴 🐎 🐴 🐴";
-            case 'z': return "🐴 🐴 🐎 🐎";
+            case 'a': return "🐴 🐎";
+            case 'b': return "🐎 🐴 🐴 🐴";
+            case 'c': return "🐎 🐴 🐎 🐴";
+            case 'd': return "🐎 🐴 🐴";
+            case 'e': return "🐴";
+            case 'f': return "🐴 🐴 🐎 🐴";
+            case 'g': return "🐎 🐎 🐴";
+            case 'h': return "🐴 🐴 🐴 🐴";
+            case 'i': return "🐴 🐴";
+            case 'j': return "🐴 🐎";
+            case 'k': return "🐎 🐴 🐎";
+            case 'l': return "🐴 🐎 🐴 🐴";
+            case 'm': return "🐎 🐎";
+            case 'n': return "🐎 🐴";
+            case 'o': return "🐎 🐎 🐎";
+            case 'p': return "🐴 🐎 🐎 🐴";
+            case 'q': return "🐎 🐎 🐴 🐎";
+            case 'r': return "🐴 🐎 🐴";
+            case 's': return "🐴 🐴 🐴";
+            case 't': return "🐎";
+            case 'u': return "🐴 🐴 🐎";
+            case 'v': return "🐴 🐴 🐴 🐎";
+            case 'w': return "🐴 🐎 🐎";
+            case 'x': return "🐎 🐴 🐴 🐎";
+            case 'y': return "🐎 🐴 🐎 🐎";
+            case 'z': return "🐎 🐎 🐴 🐴";
             case ' ': return " / ";
         }
         return "🦄";
